d99258c9e53b6bee36f8a8f0c06a9f66587fa483
Lenin-A <lenin.25meb@licet.ac.in>
2026-03-04 20:16:04 +0530

    small change

--- Program.cs
+++ Program.cs
@@ -9,10 +9,7 @@ if (string.IsNullOrWhiteSpace(connectionString))
     throw new Exception("DefaultConnection is not set! Make sure environment variable is configured.");
 
 builder.Services.AddDbContext<AppDBcontext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    )
-);
+        options.UseNpgsql("Host=dpg-d6k3lc15pdvs73dr1nt0-a;Port=5432;Database=crud_db_72sm;Username=crud_db_72sm_user;Password=uitORmgfgLhTD629wrWsJN6MpPhKmo35;SSL Mode=Require;Trust Server Certificate=true")
 
 builder.Services.AddCors(options =>
 {
