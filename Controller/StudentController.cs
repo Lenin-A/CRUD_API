7943719a29a8f48ff70b75f0c91774480215c448
Lenin-A <lenin.25meb@licet.ac.in>
2026-03-04 18:54:14 +0530

    added the api coded CRUD Opeartion

---
+++ Controller/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using EzovionAPI.Data;
+using EzovionAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EzovionAPI.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+     public class StudentController : ControllerBase
+    {
+        private readonly AppDBcontext _context;
+        public StudentController(AppDBcontext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var student = await _context.Students.ToListAsync();
+
+            return Ok(student);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id,Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(student).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(student);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+               
+            if(student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return Ok(student);
+
+        }
+    }
+}
---
+++ Data/AppDBcontext.cs
@@ -0,0 +1,20 @@
+﻿using EzovionAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EzovionAPI.Data
+{
+    public class AppDBcontext : DbContext
+    {
+        public AppDBcontext(DbContextOptions<AppDBcontext> option)
+        : base(option)
+        { }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+
+    }
+}
